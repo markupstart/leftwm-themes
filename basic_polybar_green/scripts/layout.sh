d6ca63067fac771a67d6e1af5939b92353531685
markupstart <mark@upstarters.com>
2024-01-12 21:18:17 -0500

    themes add

---
+++ basic_polybar_green/scripts/layout.sh
@@ -0,0 +1,8 @@
+#!/run/current-system/sw/bin/bash
+
+while :
+do
+  leftwm state -nq | jq -r '.workspaces[0].layout' | awk '{print tolower($0)}'
+  sleep 1
+done
+
---
+++ polybar-replacement/scripts/layout.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+while :
+do
+  leftwm state -nq | jq -r '.workspaces[0].layout' | awk '{print tolower($0)}'
+  sleep 1
+done
+
